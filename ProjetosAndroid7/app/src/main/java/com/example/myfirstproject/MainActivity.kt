28c0c800fd273c7bde423c6ea966dcbf578e26f5
Fabio Junio <124620557+SaviniFabio@users.noreply.github.com>
2023-12-22 14:09:06 -0300

    add result

--- ProjetosAndroid7/app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ ProjetosAndroid7/app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.myfirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnCalcular : Button= findViewById<Button>(R.id.btnCalcular)
+        val edtPeso: EditText = findViewById(R.id.edttext_peso)
+        val edtAltura: EditText = findViewById(R.id.edittext_altura)
+        val tvResultado : TextView = findViewById(R.id.txtView)
+
+        btnCalcular.setOnClickListener {
+
+            val alturaStr = edtAltura.text.toString()
+            val pesoStr = edtPeso.text.toString()
+
+            if (alturaStr.isNotEmpty() && pesoStr.isNotEmpty()) {
+                val altura: Float = alturaStr.toFloat()
+                val peso: Float = pesoStr.toFloat()
+
+                val alturaFinal: Float = altura * altura
+                val result: Float = peso / alturaFinal
+
+                val intent = Intent(this, ResultActivity::class.java)
+                    .apply {
+                        putExtra("EXTRA_RESULT", result)
+                    }
+                startActivity(intent)
+                } else {
+                Toast.makeText(this, "Responder todos os campos", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 }
+
--- ProjetosAndroid7/app/src/main/java/com/example/myfirstproject/ResultActivity.kt
+++ ProjetosAndroid7/app/src/main/java/com/example/myfirstproject/ResultActivity.kt
@@ -2,10 +2,46 @@ package com.example.myfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvResult = findViewById<TextView>(R.id.textview_result)
+        val tvClassificacao = findViewById<TextView>(R.id.textview_classificacao)
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+        /*TABELA IMC
+                menor que 18,5	Abaixo peso
+        entre 18,5 e 24,9	 normal
+        entre 25 e 29,9	sobrepeso
+        entre 30 e 34,9	obesidade I
+        entre 35 e 39,9	obesidade II
+        maior que 40	obesidade GRAVE III*/
+
+        var classificacao = if(result < 18.5f){
+            "ABAIXO DO PESO"
+        }else if (result in 18.5f..24.9f){
+            "NORMAL"
+        }else if (result in 25f..29.9f){
+            "SOBREPESO"
+        }else if (result in 30f..39.9f){
+            "OBESIDADE"
+        }else{
+            "OBESIDADE GRAVE!"
+        }
+        tvClassificacao.text = getString(R.string.message_classificacao, classificacao)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
